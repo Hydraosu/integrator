32ced4eaa97e08ea23f116bf9020606c0150c149
piotr.malinowski <piotr.malinowski@wfirma.pl>
2021-10-01 10:29:21 +0200

    init push

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+
+    public $user;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware(function ($request, $next) {
+
+            $this->user = Auth::user();
+
+            return $next($request);
+
+        });
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('/settings/profile', ['user' => $this->user]);
+    }
+
+    public function update(Request $request)
+    {
+        $name = $request->input('name');
+
+        DB::table('users')
+            ->where('id', $this->user->id)
+            ->update(['name' => $name]);
+
+        return redirect()->action([ProfileController::class, 'index']);
+    }
+}
---
+++ resources/views/settings/profile.blade.php
@@ -0,0 +1,31 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Profile</h1>
+@stop
+
+@section('content')
+    <form action="/settings/profile/update" method="POST">
+        @csrf
+
+        <x-adminlte-card title="Lightblue Card" theme="lightblue" theme-mode="outline"
+                         icon="fas fa-lg fa-envelope" removable>
+            <div class="row">
+                <x-adminlte-input name="name" label="Change name" placeholder="Name"
+                                  fgroup-class="col-md-4" disable-feedback value="{{ $user->name }}"/>
+            </div>
+
+            <x-adminlte-button type="submit" label="Save" theme="primary" icon="fas fa-key"/>
+        </x-adminlte-card>
+    </form>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
